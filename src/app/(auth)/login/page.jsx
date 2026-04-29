45d6350a06faa0bfa6a2dc366db852ff2e23450f
Mahedi <mahidehasansiyam123@gmail.com>
2026-04-29 12:30:31 +0600

    button style

--- src/app/(auth)/login/page.jsx
+++ src/app/(auth)/login/page.jsx
@@ -33,48 +33,50 @@ const LogingPage = () => {
   };
 
   return (
-    <div className="h-[100vh] flex justify-center items-center bg-sky-800">
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
-          <label className="label">Email</label>
-          <input
-            type="email"
-            placeholder="Email"
-            className="input"
-            {...register('email', { required: 'You must add email' })}
-          />
-          {errors.email && (
-            <span className="text-red-500">{`${errors.email.message}`}*</span>
-          )}
+    <div className="h-[100vh] flex flex-col justify-center items-center bg-sky-800">
+      <div className='bg-white'>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
+            <label className="label">Email</label>
+            <input
+              type="email"
+              placeholder="Email"
+              className="input"
+              {...register('email', { required: 'You must add email' })}
+            />
+            {errors.email && (
+              <span className="text-red-500">{`${errors.email.message}`}*</span>
+            )}
 
-          <label className="label">Password</label>
-          <input
-            type="password"
-            placeholder="Password"
-            className="input"
-            {...register('password', { required: 'You must add password' })}
-          />
-          {errors.password && (
-            <span className="text-red-500">
-              {`${errors.password.message}`}*
-            </span>
-          )}
+            <label className="label">Password</label>
+            <input
+              type="password"
+              placeholder="Password"
+              className="input"
+              {...register('password', { required: 'You must add password' })}
+            />
+            {errors.password && (
+              <span className="text-red-500">
+                {`${errors.password.message}`}*
+              </span>
+            )}
 
-          <button className="btn btn-neutral mt-4">Login</button>
-          <p>
-            Already have account?{' '}
-            <Link href="/regestration">
-              <span className="text-blue-600 underline">Regester</span>
-            </Link>{' '}
-          </p>
-        </fieldset>
-      </form>
-      <button onClick={handlegoogle}>
-        <div className="flex items-center justify-center gap-2 border-2 p-2 border-green-600 text-green-600 font-bold">
-          <FaGoogle />
-          <h2 className="r">Log in with google</h2>
-        </div>
-      </button>
+            <button className="btn btn-neutral mt-4">Login</button>
+            <p>
+              Already have account?{' '}
+              <Link href="/regestration">
+                <span className="text-blue-600 underline">Regester</span>
+              </Link>{' '}
+            </p>
+          </fieldset>
+        </form>
+        <button className="w-full  p-3 flex justify-center items-center" onClick={handlegoogle}>
+          <div className="flex items-center justify-center gap-2 border-2 p-2 rounded-2xl bg-green-600 text-white font-bold">
+            <FaGoogle />
+            <h2 className="r">Log in with google</h2>
+          </div>
+        </button>
+      </div>
     </div>
   );
 };
